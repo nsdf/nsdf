2496c1a7259575ef4037a1a484799ce07adff567
Subhasis <ray.subhasis@gmail.com>
2014-05-27 00:09:23 +0530

    Added script for profiling nan and printing the profile infor in text format.

---
+++ python/test/print_stats.py
@@ -0,0 +1,10 @@
+import pstats
+import sys
+if len(sys.argv) < 2:
+    print 'Usage: %s infile\n' % (sys.argv[0])
+    print 'Print profile info from `infile`.'
+    sys.exit(1)
+infile = sys.argv[1]
+pstats.Stats(infile).strip_dirs().sort_stats("name", "cumulative").print_stats()
+
+
