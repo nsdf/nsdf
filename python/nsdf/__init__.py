dc57e96068ac122e489f52704d9682fe4247b1a4
Subhasis <ray.subhasis@gmail.com>
2014-07-15 18:32:10 +0530

    minor update to include model.py in module __init__

--- python/nsdf/__init__.py
+++ python/nsdf/__init__.py
@@ -1,4 +1,6 @@
 """NSDF writing and reading utilities."""
 __author__ = 'Subhasis Ray'
 __version__ = '0.1'
-from .NSDFWriter import NSDFWriter as writer
+from .model import ModelComponent
+
+# from .NSDFWriter import NSDFWriter as writer
