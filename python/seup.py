65af5aeb7842a5851d088760b985ef11a860617e
Subhasis Ray <ray.subhasis@gmail.com>
2014-07-31 01:15:42 +0530

    Added a setup.py.

---
+++ python/seup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), '..', fname)).read()
+
+setup(
+    name = "nsdf",
+    version = "0.1",
+    author = "Subhasis Ray",
+    author_email = "ray dot subhasis at gmail dot com",
+    description = ("NSDF (Neuroscience Simulation Data Format)"),
+    license = "GPL 3",
+    keywords = "example documentation tutorial",
+    url = "http://github.com/subhacom/nsdf",
+    packages=['nsdf', 'test'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 1 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GPL License",
+    ],
+)
