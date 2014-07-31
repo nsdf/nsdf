e1548b626c8548d59a4d8341a1043321c60fb0d7
Subhasis <ray.subhasis@gmail.com>
2014-07-31 11:17:36 +0530

    Updated documentation of constants.

--- nsdf/constants.py
+++ nsdf/constants.py
@@ -67,16 +67,20 @@ STATIC = 'static'
 class dialect(object):
     """Enumeration of different dialects of NSDF.
 
-    VLEN: nonuniform and event data are in vlen 2d datasets
+    VLEN: 
+        nonuniform and event data stored in vlen 2d datasets.
 
-    ONED: nonuniform and event data are in 1d datasets
+    ONED: 
+        nonuniform and event data stored in 1d datasets.
      
-    NANPADDED: nonuniform and event data in regular 2d
-        datasets with NaN padding.
+    NANPADDED: 
+        nonuniform and event data in stored in regular 2d datasets
+        with NaN padding.
 
-    NUREGULAR: nonuniform data has shared sampling times. Thus
-        nonuniform data goes into regular 2D datasets.  In this case the
-        events are stored in 1D datasets.
+    NUREGULAR: 
+        nonuniform datasets have shared sampling times. Thus
+        nonuniform data goes into regular 2D datasets. In this case
+        the events are stored in 1D datasets.
 
     """
     VLEN = 0    
