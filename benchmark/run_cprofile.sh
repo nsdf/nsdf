7c11b956e4e0b5a8cb73fceb317e75ac5d40d22f
Subhasis <ray.subhasis@gmail.com>
2014-09-05 16:44:12 +0530

    Added python scripts to run and print profile stats. Fixed a bug in incremental writing benchmark.

--- benchmark/run_cprofile.sh
+++ benchmark/run_cprofile.sh
@@ -1,10 +1,13 @@
 # Run cprofile for profiling the benchmark writer
 # Author: Subhasis Ra, Date: Wed Sep  3 20:28:31 IST 2014
+MINCOL=1000
+MAXCOL=10000
+VARS=10
 
 # fixed, uncompressed
 python -m cProfile -o ONED_fixed_uncompressed.prof benchmark_writer.py -m 100000 -n 200000 -d oned &> profile_out_ONED_fixed_uncompressed.txt
 python -m cProfile -o VLEN_fixed_uncompressed.prof benchmark_writer.py  -m 100000 -n 200000 -d vlen &> profile_out_VLEN_fixed_uncompressed.txt
-python -m cProfile -o AN_fixed_uncompressed.prof benchmark_writer.py  -m 100000 -n 200000 -d nan &> profile_out_NAN_fixed_uncompressed.txt
+python -m cProfile -o NAN_fixed_uncompressed.prof benchmark_writer.py  -m 100000 -n 200000 -d nan &> profile_out_NAN_fixed_uncompressed.txt
 
 # fixed, compressed
 python -m cProfile -o ONED_fixed_compressed.prof benchmark_writer.py -c  -m 100000 -n 200000 -d oned &> profile_out_ONED_fixed_compressed.txt
