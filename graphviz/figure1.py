b751ae1b00b43811eacf9e6fdc59cb9a48bfc2c0
Chaitanya  Chintaluri <cchintaluri@PC414-C842.(none)>
2014-10-02 16:58:33 +0200

    fixing commit, skipped essential file

--- graphviz/figure1.py
+++ graphviz/figure1.py
@@ -6,12 +6,12 @@
 # for the convenience of the user.
 
 # Use matplotlib and pygraphviz
-
+import re
 import matplotlib.pyplot as plt
 import pygraphviz as pgv
 import webcolors as wc
 
-width_box = 1
+width_box = 1.0
 edge_width = 2.0
 
 font_name = 'Arial'
@@ -20,109 +20,62 @@ font_size = 12.0
 subgrp_shape = 'tab' #http://www.graphviz.org/doc/info/shapes.html#d:style
 leafnde_shape = 'note'
 
-NODE_0_C = wc.name_to_hex('white') #root
-NODE_1_C = wc.name_to_hex('skyblue') #data/map/model
-NODE_2_C = wc.name_to_hex('wheat') #uniform/static
-NODE_3_C = wc.name_to_hex('lightgreen') #population
-NODE_4_C = wc.name_to_hex('sandybrown') #parameter
-NODE_5_C = wc.name_to_hex('lightgrey') #oned
+NODE_0 = wc.name_to_hex('white') #root
+NODE_1 = wc.name_to_hex('skyblue') #data/map/model
+NODE_2 = wc.name_to_hex('wheat') #uniform/static
+NODE_3 = wc.name_to_hex('lightgreen') #population
+NODE_4 = wc.name_to_hex('sandybrown') #parameter
+NODE_5 = wc.name_to_hex('lightgrey') #oned
 
-def add_children(G, parent_node, child_list, color=None, end_node=False):
-    child_node_list = []
-    dummy_point_list = []
-    children = []
-    for child_idx, child in enumerate(child_list):
-        child_x = child+'_'+str(G.order()) 
-        children.append(child_x)
-        if end_node:
-            G.add_node(child_x, label=child, width=width_box, shape=leafnde_shape, style='filled', concentrate=True, fillcolor=color, 
-                       fontname=font_name, fontsize=font_size)
-        else:
-            G.add_node(child_x, label=child, width=width_box, shape=subgrp_shape, style='filled', concentrate=True, fillcolor=color, 
+NODE_COLOR = [NODE_0, NODE_1, NODE_2, NODE_3, NODE_4, NODE_5]
+
+def add_child(G, parent_node, child, color=None, end_node=False):
+    if parent_node=='/':
+        child_x = parent_node+child 
+    else:
+        child_x = parent_node+'/'+child 
+    G.add_node(child_x+'_point', shape='point', width=0.05)
+    child_point_node = G.get_node(child_x+'_point')
+    G.add_edge(parent_node, child_point_node, weight=2, penwidth=edge_width, arrowsize=0.0, arrowhead=None, constraint=False)
+    if end_node:
+        G.add_node(child_x, label=child, width=width_box, shape=leafnde_shape, style='filled', concentrate=True, fillcolor=color, 
                    fontname=font_name, fontsize=font_size)
-        G.add_node(child_x+'_point', shape='point', width=0.05)
-        child_node = G.get_node(child_x)
-        child_point_node = G.get_node(child_x+'_point')
-        G.add_edge(child_point_node, child_node, penwidth=edge_width, weight=2)
-        child_node_list.append(child_node)
-        dummy_point_list.append(child_point_node)
+    else:
+        G.add_node(child_x, label=child, width=width_box, shape=subgrp_shape, style='filled', concentrate=True, fillcolor=color, 
+                   fontname=font_name, fontsize=font_size)
+    child_node = G.get_node(child_x)
+    G.add_edge(child_point_node, child_node, penwidth=edge_width, weight=3)
+    H = G.subgraph([child_point_node, parent_node], rank='same', constraint=False)
+    H = G.subgraph([child_point_node, child], rank='same')
+    return child_node
 
-    G.add_edge(parent_node, dummy_point_list[0], arrowhead=None, arrowsize=0.0, penwidth=edge_width)
-    for idx in range(len(dummy_point_list)-1):
-        G.add_edge(dummy_point_list[idx], dummy_point_list[idx+1], arrowhead=None, arrowsize=0.0, penwidth=edge_width, weight=1)
-    H = G.subgraph(child_node_list, rank='same')
-    H = G.subgraph(dummy_point_list+[parent_node], rank='same')
-    return children
-
-# def unwrap_path(path_list):
-#     all_dict = {}
-#     for path in path_list:
-#         all_eles = path.split('/')[1:]
-#         ii=0
-#         for ii,jj in enumerate(all_eles):
-#             try:
-#                 child = all_dict[jj]
-#             except KeyError:
-#                 all_dict[jj] = {}
-#     print all_dict
-
-# test = ['/data/uniform/pop1/Vm',
-#         '/data/uniform/pop1/Im',
-#         '/data/events/pop1/spikes',
-#         '/map/uniform/pop1']
-
-# unwrap_path(test)                
-                
-
-def figure1a():
-    G = pgv.AGraph(strict=True,directed=True, rankdir='LR',ranksep='0.25', splines=False, nodesep=0.25)#, rank='source')
-    G.add_node('ROOT', label='ROOT', shape=subgrp_shape, style='filled', concentrate=True, width=width_box,
-               fontname=font_name, fontsize=font_size, fillcolor=NODE_0_C) #root level
-    children = add_children(G, 'ROOT', ['model', 'map', 'data'], NODE_1_C) #  #parent level (model/data/map)
-    add_children(G, children[2], ['static', 'events', 'uniform', 'nonuniform'], NODE_2_C) #  #Child level (uniform/static)
+def gen_figure(dir_list):
+    G = pgv.AGraph(strict=True, directed=True, rankdir='LR', ranksep='0.25', splines=False, nodesep=0.25)
+    G.add_node('/', label='ROOT', shape=subgrp_shape, style='filled', concentrate=True, width=width_box,
+               fontname=font_name, fontsize=font_size, fillcolor=NODE_0)
+    for path in dir_list:
+        if path.startswith('/'):
+            pass
+        else:
+            path = '/'+path #starting with root
+        path_idx = [m.start() for m in re.finditer('/', path)]
+        sub_dirs = path.split('/')[1:] #skip the first
+        for ii,sub_folder in enumerate(sub_dirs):
+            try: 
+                dummy = G.get_node(path[:path_idx[ii]]+'/'+sub_folder)
+                #print 'Node already exists:', path[:path_idx[ii]]+'/'+sub_folder
+                pass
+            except KeyError:
+                if ii==0:
+                    add_child(G, '/', sub_folder, NODE_COLOR[ii+1])
+                else:
+                    add_child(G, path[:path_idx[ii]], sub_folder, NODE_COLOR[ii+1])
     G.layout('dot')
-    G.draw('figure1a.svg')
+    G.draw('figure2_lhs.svg')
 
-def figure1b():
-    G = pgv.AGraph(strict=True,directed=True, rankdir='LR',ranksep='0.25', splines=False, nodesep=0.25)#, rank='source')
-    G.add_node('data', label='data', shape=subgrp_shape, style='filled', concentrate=True, width=width_box,
-               fontname=font_name, fontsize=font_size, fillcolor=NODE_1_C) 
-    children = add_children(G, 'data', ['uniform'], NODE_2_C)
-    children = add_children(G, children[0], ['population0', 'population1'], NODE_3_C) #  #Child level (uniform/static)
-    add_children(G, children[0], ['Vm', 'Im', 'Gk', 'Ik'], NODE_4_C, True)
-    add_children(G, children[1], ['Vm'], NODE_4_C, True)
-    G.layout('dot')
-    G.draw('figure1b.svg')
+dir_list = ['/event/kill/me',
+            '/data/list/tada/hello',
+            'event/tell/this',
+            '/data/list/tada/meow']
 
-def figure1c():
-    G = pgv.AGraph(strict=True,directed=True, rankdir='LR',ranksep='0.25', splines=False, nodesep=0.25)#, rank='source')
-    G.add_node('data', label='data', shape=subgrp_shape, style='filled', concentrate=True, width=width_box,
-               fontname=font_name, fontsize=font_size, fillcolor=NODE_1_C) 
-    children = add_children(G, 'data', ['event'], NODE_2_C) 
-    children = add_children(G, children[0], ['population0', 'population1'], NODE_3_C) #  #Child level (uniform/static)
-    children_sp1 = add_children(G, children[0], ['spike'], NODE_4_C)
-    add_children(G, children_sp1[0], ['spikedataset0','spikedataset1','spikedataset2'], NODE_5_C, True)
-    children_sp2 = add_children(G, children[1], ['spike'], NODE_4_C)
-    add_children(G, children_sp2[0], ['spikedataset0'], NODE_5_C, True)
-    G.layout('dot')
-    G.draw('figure1c.svg')
-
-def figure1d():
-    G = pgv.AGraph(strict=True,directed=True, rankdir='LR',ranksep='0.25', splines=False, nodesep=0.25)#, rank='source')
-    G.add_node('data', label='data', shape=subgrp_shape, style='filled', concentrate=True, width=width_box,
-               fontname=font_name, fontsize=font_size, fillcolor=NODE_1_C) 
-    children = add_children(G, 'data', ['nonuniform'], NODE_2_C) 
-    children = add_children(G, children[0], ['neurons', 'AMPA', 'GABA'], NODE_3_C) #  #Child level (uniform/static)
-    children_sp1 = add_children(G, children[0], ['Im'], NODE_4_C)
-    add_children(G, children_sp1[0], ['soma0','soma1'], NODE_5_C, True)
-    children_sp2 = add_children(G, children[1], ['Gk'], NODE_4_C)
-    add_children(G, children_sp2[0], ['soma_0_ampa', 'dend_0_ampa'], NODE_5_C, True)
-    children_sp2 = add_children(G, children[2], ['Gk'], NODE_4_C)
-    add_children(G, children_sp2[0], ['dend_0_gaba', 'dend_4_gaba'], NODE_5_C, True)
-    G.layout('dot')
-    G.draw('figure1d.svg')
-
-figure1a()
-figure1b()
-figure1c()
-figure1d()
+gen_figure(dir_list)
